dd68906616d2fefb43307fe6b3b6e1650ce259bf
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2025-02-22 12:32:05 +0530

    Update stat_playground_v0.1.py

--- stat_playground_v0.1.py
+++ stat_playground_v0.1.py
@@ -79,7 +79,7 @@ st.markdown(
     </style>
 
     <div class="footer">
-        <p>© 2025 | Created by : <span class="highlight">Avijit Chakraborty</span> | Prepared by: <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a></p> <span class="highlight">Thank you for visiting the app | Unauthorized uses or copying is strictly prohibited | For best view of the app, please zoom out the browser to 75%.</span>
+        <p>© 2025 | Created by : <span class="highlight">Avijit Chakraborty</span> | <a href="mailto:avijit.mba18@gmail.com"> 📩 <span class="highlight">Thank you for visiting the app | Unauthorized uses or copying is strictly prohibited | For best view of the app, please zoom out the browser to 75%.</span>
     </div>
     """,
     unsafe_allow_html=True)
